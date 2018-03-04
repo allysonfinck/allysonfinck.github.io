aec130c3a7ae5efc9459ecfacc8bc8f4a87c668a
Allyson Finck <allyson.finck@gmail.com>
2018-03-04 16:44:25 -0500

    appended hole randomly to course

--- js/app.js
+++ js/app.js
@@ -5,28 +5,27 @@ const gridSquares = [];
 
 //make golf course and create array of square ids
 const createGrid = () =>{
-  for (let i = 1; i < 100; i++){
+  for (let i = 1; i <= 100; i++){
   const $div = $('<div>').addClass('gridSquare').attr('id', 'square' + i).appendTo('#grid');
   gridSquares.push(i);
   }
 }
 
-console.log(gridSquares);
-
 //find random grid square
-const randSquareId = () =>{
-  console.log('#square' + Math.floor(Math.random()*(gridSquares.length)));
+const randSquare = () =>{
+  const randSquareId = '#square' + Math.floor(Math.random()*(gridSquares.length));
+  const createHole = () =>{
+    const $hole = $('<div>').addClass('hole').appendTo(randSquareId);
+  }
+  createHole();
 }
 
 //create and locate hole
-const createHole = () =>{
-  const $hole = $('<div>').addClass('hole').appendTo('body');
-}
+
 
 $(()=>{
 
   createGrid();
-  randSquareId();
-  createHole();
+  randSquare();
 
 });
