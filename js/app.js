439f24c9de80f0a990d8d2de5d59fd89a174e5f6
Allyson Finck <allyson.finck@gmail.com>
2018-03-04 13:11:31 -0500

    created game doc and grid

---
+++ js/app.js
@@ -0,0 +1,20 @@
+console.log('hi!');
+console.log($);
+
+//make golf course
+const createGrid = () =>{
+  for (let i = 1; i < 100; i++){
+  const $div = $('<div>').addClass('gridSquare').appendTo('#grid');
+  }
+}
+
+//create and locate hole
+const createHole = () =>{
+  
+}
+
+$(()=>{
+
+  createGrid();
+
+});
