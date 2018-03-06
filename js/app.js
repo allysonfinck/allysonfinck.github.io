5732e95e7b87346c75b3fa175c5ea9613de0874a
Allyson Finck <allyson.finck@gmail.com>
2018-03-06 13:34:17 -0500

    erased grid-added source-working on strength meter

--- js/app.js
+++ js/app.js
@@ -2,7 +2,7 @@ console.log('hello');
 console.log($);
 
 const gridSquares = [];
-const meter = ['#bar1', '#bar2', '#bar3', '#bar4', '#bar5', '#bar6', '#bar7', '#bar8', '#bar9', '#bar10'];
+// const meter = ['#bar1', '#bar2', '#bar3', '#bar4', '#bar5', '#bar6', '#bar7', '#bar8', '#bar9', '#bar10'];
 //make golf course and create array of square ids
 const createGrid = () =>{
   for (let i = 1; i <= 100; i++){
@@ -38,10 +38,7 @@ const moveLeft = ()=>{
 //determine strength of shot
 const increaseStrength = ()=>{
   $('#bar1').css('box-shadow', '0px 0px 15px 5px rgba(255, 255, 190, .75)');
-    // for(i = 0; i < meter.length; i++){
-    //   console.log(meter[i]);
-    // //   $('meter[i]').css('box-shadow', '0px 0px 15px 5px rgba(255, 255, 190, .75)');
-    // }
+
 }
 
 $(()=>{
